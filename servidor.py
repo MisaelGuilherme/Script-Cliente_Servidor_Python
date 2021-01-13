53502632cafc587d62ce27443a3a04897294f285
Misael Guilherme <63935572+MisaelGuilherme@users.noreply.github.com>
2021-01-13 16:27:29 -0300

    Criando procedimentos de conexões do servidor

---
+++ servidor.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+import socket
+
+try:
+    
+    #socket.socket == realizar todo o procedimento | socket.DF_NET == ipv4 | SOCK_STREAM == TCP
+    servidor = socket.socket()
+    
+    #bind serve para amarrar o ip e a porta
+    servidor.bind(('', 3315))
+    
+    #Número de clientes que vão ficar em filas
+    servidor.listen( 5 )
+
+    
+
+    #Iniciando um laço de repetição para a porta sempre ficar escutando e esperando o cliente
+    while True:
+        
+        
+        
+        #Aceitando conexão com o cliente ao se conectar, receberá o ip e a porta
+        client, address = servidor.accept()
+        
+        
+
+except: print('Servidor desconectado da Rede')
